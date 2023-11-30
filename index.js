40a21560256d54ea456862f9d327173171245f19
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-11-30 11:38:46 +0530

    commit2

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require("express");
-const cors = require("cors");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const app = express();
+const server = http.createServer((req, res) => {
+  // Check if the request is for the root path
+  if (req.url === "/") {
+    // Read the content of the HTML file
+    const filePath = path.join(__dirname, "index.html");
 
-app.use(cors());
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("hello world");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading HTML file:", err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        // Set the content type to HTML
+        res.writeHead(200, { "Content-Type": "text/html" });
+        // Send the HTML content as the response
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other paths (e.g., 404 Not Found)
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/`);
+});
