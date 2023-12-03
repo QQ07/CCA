902ed66d996f53c444d0866852aedb3a709d94b6
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-12-03 16:05:59 +0530

    port 3000

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 80;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
 });
