b60218276a47f5cfaea9424c7305073162f1f770
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-12-03 17:25:23 +0530

    node app for payroll generation

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  // Check if the request is for the root path
-  if (req.url === "/") {
-    // Read the content of the HTML file
-    const filePath = path.join(__dirname, "index.html");
+const app = express();
+const PORT = 3000;
 
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading HTML file:", err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
-      } else {
-        // Set the content type to HTML
-        res.writeHead(200, { "Content-Type": "text/html" });
-        // Send the HTML content as the response
-        res.end(data);
-      }
-    });
+// Root route
+app.get("/", (req, res) => {
+  // Read the content of the HTML file
+  const filePath = path.join(__dirname, "index.html");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading HTML file:", err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      // Set the content type to HTML
+      res.status(200).send(data);
+    }
+  });
+});
+
+
+
+// Simulated employee data store
+const employeeData = {
+  101: {
+    baseSalary: 50000,
+  },
+  102: {
+    baseSalary: 60000,
+  },
+};
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Endpoint to get employee salary by ID
+app.get("/getSalary/:employeeId", (req, res) => {
+  const employeeId = req.params.employeeId;
+  const employee = employeeData[employeeId];
+
+  if (employee) {
+    res.json({ baseSalary: employee.baseSalary });
   } else {
-    // Handle other paths (e.g., 404 Not Found)
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
+    res.status(404).json({ error: "Employee not found" });
   }
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
 });
