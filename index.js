32f75a209623705531785f70cc0ceb4732237c50
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-12-07 15:28:23 +0530

    test1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
-const PORT = 3000;
+const PORT = 3001;
 
 // Root route
 app.get("/", (req, res) => {
